0fd3f75b05f47c5647953fc3c62061f9cdc555e8
Manish1542545211 <manishkumar9466733@gmail.com>
2025-07-03 11:44:23 +0530

    more optimised(Kadanes algorithm)

---
+++ Datastructures/Maxsubarryas3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+//optimised version using Kadanes Algorithm
+void MaxsubarraysSum1(int arr[],int n){
+    int Maxsum=INT_MIN;
+    int cursum=0;
+  for (int i=0;i<n;i++){
+    cursum+=arr[i];
+    Maxsum=max(Maxsum,cursum);
+    if(cursum<0){
+        cursum=0;
+    }
+
+
+  }
+  cout<<"max sum of all the subarrays is ="<<cursum<<endl;
+
+}
+
+
+int main(){
+    int arr[]={2,-3,6,-5,4,2};
+    int n=sizeof(arr)/sizeof(n);
+    MaxsubarraysSum1(arr,n);
+    
+}
