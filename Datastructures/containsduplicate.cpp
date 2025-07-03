31dcb190f0c2aa9a61ec34f2d3b63a527d97e7d1
Manish1542545211 <manishkumar9466733@gmail.com>
2025-07-03 19:17:10 +0530

    do again after doing sorting

--- Datastructures/containsduplicate.cpp
+++ Datastructures/containsduplicate.cpp
@@ -2,28 +2,18 @@
 #include<limits.h>
 using namespace std;
 bool containsduplicate(int nums[],int n){
-     int count=0;
      
-     for(int i=0;i<n;i++){
-        count=0;
-        for(int j=0;j<n;j++){
-            if(nums[i]==nums[j] ){
-                count++;
+     for(int i=1;i<n;i++){
+       if(nums[i-1]==nums[i]){
+        return true;
+       }
+       
         
-                
-            }
-            if(count>=2){
-                return true;
-            }
-        }
-        
-
-
      }
      return false;
 }
 int main(){
-    int nums[6]={1,2,3,4,5,5};
+    int nums[6]={1,2,3,4,};
     int n=sizeof(nums)/sizeof(int);
     cout<<containsduplicate(nums,n)<<endl;
 
