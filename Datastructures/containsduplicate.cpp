7d681b7ac84b56175879d2f5ec9ec0b7c4553a26
Manish1542545211 <manishkumar9466733@gmail.com>
2025-07-04 11:05:07 +0530

    The leet code problem about containsduplicate

--- Datastructures/containsduplicate.cpp
+++ Datastructures/containsduplicate.cpp
@@ -1,23 +1,20 @@
 #include<iostream>
 #include<limits.h>
 using namespace std;
-bool containsduplicate(int nums[],int n){
-     
-     for(int i=1;i<n;i++){
-       if(nums[i-1]==nums[i]){
-        return true;
-       }
-       
-        
-     }
-     return false;
-}
+#include <vector>
+#include<algorithm>
+bool containsDuplicate(vector<int>&nums){
+  sort(nums.begin(),nums.end());
+  for(int i=1;i<nums.size();i++){
+    if(nums[i-1]==nums[i]){
+      return true;}
+    }return false;
+  }
+
+
 int main(){
-    int nums[6]={1,2,3,4,};
-    int n=sizeof(nums)/sizeof(int);
-    cout<<containsduplicate(nums,n)<<endl;
-
-
+  vector<int>nums={1,2,3,4,5,6,5,4,4,4,5};
+  cout<<containsDuplicate(nums)<<endl;
 
 
 }
