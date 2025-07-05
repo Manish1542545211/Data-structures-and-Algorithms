471d8456df1f31f2e7d0e7cf7275a61a6e9eacc1
Manish1542545211 <manishkumar9466733@gmail.com>
2025-07-05 11:02:01 +0530

    bubble sort algortihm

---
+++ Datastructures/bubblesortalgo.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+//bubble sort algorithm
+void print(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    
+    }
+}
+void bubblesort(int arr[],int n){
+    bool isswap=false;
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                isswap=true;
+            }
+        }
+        if(isswap==false){
+            return;
+        }
+    }
+    print(arr,n); 
+}
+int main(){
+     int array[]={4,5,1,3,2};
+     int n=sizeof(array)/sizeof(int);
+     bubblesort(array,5);
+     return 0;
+}
