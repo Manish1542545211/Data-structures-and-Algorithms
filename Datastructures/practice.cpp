69f47085727d51d4a99229c7d9679a93a96669cd
Manish1542545211 <manishkumar9466733@gmail.com>
2025-07-18 13:27:11 +0530

    pair sum 167th leetcode question

--- Datastructures/practice.cpp
+++ Datastructures/practice.cpp
@@ -1,44 +1,21 @@
 #include<iostream>
 #include<limits.h>
 #include<algorithm>
+#include<vector>
 using namespace std;
-void print(int arr[],int n){
-    for (int i=0;i<n;i++){
-    cout<<arr[i]<<",";
-    }
-}
 
-void countingsort(int arr[],int n){
-     int frequancy[10000]={0};
-     int Max=INT_MIN,Min=INT_MAX;
-     for(int i=0;i<n;i++){
-        Max=max(Max,arr[i]);
-        Min=min(Min,arr[i]);
-     }
-     for(int i=0;i<n;i++){
-        frequancy[arr[i]]++;
-     }
-
-     for(int i=Min,j=0;i<Max;i++){
-         while(frequancy>0){
-             arr[j++]=i;
-             frequancy[i]--;
-
-         }
-
-     }
-     print(arr,n);
-
-
-}
 
 
 
 
 int main(){
-    int arr[]={3,6,2,1,8,7,4,5,3,1};
-    int n=sizeof(arr)/sizeof(int);
-    countingsort(arr,n);
+    vector<int>vec1={1,2,3,4};
+    cout<<"vector size"<<vec1.size()<<endl;
+    cout<<"vector capacity"<<vec1.capacity()<<endl;
+    vec1.push_back(5);
+    cout<<"vector size"<<vec1.size()<<endl;
+    cout<<"vector capacity"<<vec1.capacity()<<endl;
+
 
     
 
