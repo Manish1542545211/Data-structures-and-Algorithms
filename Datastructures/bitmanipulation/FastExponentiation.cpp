4c18cf0b6c2ff895b0267f04bc4b84dddb50ec1c
Manish1542545211 <manishkumar9466733@gmail.com>
2025-07-22 12:50:11 +0530

    fast exponentiation with the bit manipulation

---
+++ Datastructures/bitmanipulation/FastExponentiation.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void FastExpo(int n,int x){
+    int ans =1;
+    while(n>0){
+       int lastdigit=n&1;
+       if(lastdigit){
+        ans=ans*x;
+       }
+       x=x*x;
+       n=n>>1;
+
+    }
+    cout<<ans<<endl;
+}
+int main(){
+    FastExpo(5,3);
+}
---
+++ Datastructures/bitmanipulation/clearibts.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void clearibits(int num,int i){
+    int bitmask=(~0)<<i;
+    num=num&bitmask;
+    cout<<num<<endl;
+
+}
+int main(){
+     clearibits(15,2);
+
+
+
+
+
+}
