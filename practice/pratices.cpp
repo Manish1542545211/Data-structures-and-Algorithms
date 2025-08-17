d930eed4e80b3b45792f27bc09b72aeca17e6b0a
Manish1542545211 <manishkumar9466733@gmail.com>
2025-08-17 19:03:29 +0530

    commiting

--- practice/pratices.cpp
+++ practice/pratices.cpp
@@ -5,25 +5,7 @@
 #include<algorithm>
 #include<vector>
 using namespace std;
-void search(int arr[],int i,int n,int Key){
- if(i==n){
-  return ;
- }
- if(arr[i]==Key){
-  cout<<i<<",";
- }
- return search(arr,i+1,n,2);
-}
 
 
 
 
-int main(){
-  int arr[]={3,2,4,5,6,2,7,2,2};
-  int n=sizeof(arr)/sizeof(arr[0]);
-  search(arr,0,n,2);
-
-  
-  return 0;   
-
-}
