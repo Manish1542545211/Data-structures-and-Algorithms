3e58e5e8666434ee9c022204f363332fec7b7087
Manish1542545211 <manishkumar9466733@gmail.com>
2025-09-09 16:11:55 +0530

    commiting

--- Python/Day.py
+++
@@ -1,3 +0,0 @@
-A=float(input("Enter your Frist Number"))
-B=float(input("Enter your 2nd Number"))
-print(A>=B)
--- practice/pratices.cpp
+++ practice/pratices.cpp
@@ -6,16 +6,16 @@
 #include<vector>
 #include<climits>
 using namespace std;
-int MaxSubarraySum(int array[],int n){
-    
-}
+
     
 
-int main(){
-   int array[]={2,-3,6,-5,4,2};
-   int n=sizeof(array)/sizeof(array[0]);
-   MaxSubarraySum (array,n);
+int main(){ 
+  std::string greeting="hello";
+  std::cout<<greeting;
+
+  return 0;
 }
+   
 
 
 
