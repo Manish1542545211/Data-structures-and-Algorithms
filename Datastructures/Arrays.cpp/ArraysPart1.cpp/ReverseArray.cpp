48bd8b2e1ae660cc1bc6448373743040ea6bddcd
Manish1542545211 <manishkumar9466733@gmail.com>
2025-10-26 21:45:05 +0530

    26/10/2025

--- Datastructures/Arrays.cpp/ArraysPart1.cpp/ReverseArray.cpp
+++ Datastructures/Arrays.cpp/ArraysPart1.cpp/ReverseArray.cpp
@@ -1,20 +1,50 @@
 #include<iostream>
 using namespace std;
-
 int main(){
-    int array1[]={5,4,3,9,2};
-    int n=sizeof(array1)/sizeof(array1[0]);
-    int array2[n];
-    int j=0;
-    for(int i=n-1;i>=0;i++){
-        array2[j]=array1[i];
-        j++;
+       //Simple O(n) Time complexity with extra space
+//     int array1[]={5,4,3,9,2};
+//     int n=sizeof(array1)/sizeof(array1[0]);
+//     int array2[n];
+//     int j=0;
+//    for(int i=0;i<n;i++){
+//     array2[i]=array1[n-1-i];
+//    }
+//     for(int i=0;i<n;i++){
+//         cout<<array2[i]<<endl;
+//     }
+//     for(int i=0;i<n;i++){
+//         array1[i]=array2[i];
+
+//     }
+//     for(int i=0;i<n;i++){
+//         cout<<array1[i]<<endl;
+//     }
+
+ // Two Pointer approach(Without Extra Space)
+    int array[]={5,4,3,9,2};
+    int n=sizeof(array)/sizeof(array[0]);
+    int start=0,end=n-1;
+    while(start<end){
+        int temp=array[start];
+        array[start]=array[end];
+        array[end]=temp;
+        start++;
+        end--;
     }
-    for(int i=0;i<n;i++){
-        cout<<array2[i]<<endl;
-    } 
-      
+    //slight improvement
+//     for (int start = 0, end = n - 1; start < end; start++, end--) {
+//     int temp = array[start];
+//     array[start] = array[end];
+//     array[end] = temp;
+// }
+
+ 
+    for (int value:array){
+        cout<<value<<" ";
+    }
+
+    
+
+
     return 0;
-
-
 }
