daccc16fbf9f0a6daf3c17395b37f44ffd5fe2fc
Manish1542545211 <manishkumar9466733@gmail.com>
2025-10-27 10:05:50 +0530

    Some new Addition

---
+++ Datastructures/Arrays.cpp/ArraysPart1.cpp/ArrayPointer.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+// void printArr(int *ptr,int n){
+//     //Pointer arithmatic 2
+//     for(int i=0;i<n;i++){
+//         cout<<*(ptr+i)<<endl;
+//     }
+// }
+
+
+int main(){
+    // int x=10;
+    // int *ptr=&x;
+
+    // int y=25;
+    // ptr=&y;
+    // cout<<*ptr<<"\n";//This pointer value can be changed
+
+    // 
+    // int arr[5];
+    // cout<<arr<<endl;
+    // int y=25;
+    // arr=&y;//constant value of arr can not be changed 
+
+    //Pointer arithmatic 1
+//    int  a=25;
+//    int *ptr=&a;
+//    cout<<ptr<<"\n";
+//    ptr++;
+//    cout<<ptr<<"\n";
+    //   ptr--;
+    //   cout<<ptr<<"\n";
+
+    //Pointer arithmatic 2
+    // int a=5;
+    // int *ptr=&a;
+    // cout<<ptr<<"\n";
+    // cout<<ptr+3<<"\n";   
+
+    // int a=5;
+    // int *ptr=&a;
+    // cout<<ptr<<"\n";
+    // ptr=ptr+3;
+    // cout<<(ptr-3)<<"\n";
+
+    // int arr[]={1,2,3,4,5};
+    // int n=sizeof(arr)/sizeof(arr[0]);
+    // printArr(arr,n);
+
+
+    //Pointer arithmatic 3
+    //ptr1+ptr2 invalid operation 
+    //ptr1-ptr2 valid operation 
+    // int a=5;
+    // int *ptr1=&a;
+    // int *ptr2=ptr1+3;
+
+    // cout<<ptr2<<"\n";
+    // cout<<ptr1<<"\n";
+    // cout<<ptr2-ptr1<<"\n"; gives number of int or any type spaces between two pointer 
+
+
+    // int arr[20]={1,2,3,4,5,6};
+    // int *ptr1=arr;
+    // int *ptr2=ptr1+3;
+    // cout<<*ptr2<<"\n";
+    // cout<<*ptr1<<"\n";
+    //cout<<ptr2-ptr1<<"\n";
+ 
+    //Pointer arithematic 4
+    // int arr[20]={1,2,3,4,5,6};
+    // int *ptr1=arr;
+    // int *ptr2=ptr1+3;
+    // cout<<(ptr2>ptr1)<<"\n";
+    // cout<<(ptr2==ptr1)<<"\n";
+    
+    
+    return 0;
+
+
+}
--- Datastructures/Practice/python.py
+++
@@ -1,7 +0,0 @@
-from functools import reduce
-mylist=[1,2,3,4]
-def dummy(a,b):
-    return a*b
-
-reducevalue=reduce(dummy,mylist)
-print(reducevalue)
