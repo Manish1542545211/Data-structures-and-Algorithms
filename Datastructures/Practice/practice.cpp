b365cdc53e277ab55f8e1c594dc82e205a379a53
Manish Kumar <manishkumar9466733@gmail.com>
2025-11-24 22:03:34 +0530

    commit

--- Datastructures/Practice/practice.cpp
+++ Datastructures/Practice/practice.cpp
@@ -1,38 +1,19 @@
 #include<iostream>
-#include<string>
-#include<algorithm>
-#include<limits.h>
 using namespace std;
-void maxprofit(int *prices,int n){
-  int bestBuy[100000];
-  bestBuy[0]=INT_MAX;
-   for(int i=1;i<n;i++){
-    bestBuy[i]=min(bestBuy[i-1],prices[i-1]);
-    
-    }
-   int maxProfit=0;
-   for(int i=0;i<n;i++){
-     int currentprofit=prices[i]-bestBuy[i];
-     maxProfit=max(maxProfit,currentprofit);
-   }
-   cout<<"max profit="<<maxProfit<<endl;
-
-
-
-
-
-}
 
 
 int main(){
-  int prices[6]={7,1,5,3,6,4};
-  int n=sizeof(prices)/sizeof(int);
-  maxprofit(prices,n);
+    int student[3][3]={{100,100,100}
+                      ,{85,74,89},
+                       {63,72,65}};
+    cout<<student[1][1]<<endl;
 
 
-  
 
 
 
-  return 0;
+
+
+    return 0;
 }
+
