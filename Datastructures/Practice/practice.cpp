19d2aa0ca37d4a0f76277fc29eec2c67368d3e9b
Manish Kumar <manishkumar9466733@gmail.com>
2025-11-27 16:09:30 +0530

    new changes

--- Datastructures/Practice/practice.cpp
+++ Datastructures/Practice/practice.cpp
@@ -1,35 +1,31 @@
-#include<iostream>
-using  namespace std;
-void selectionsort(int arr[],int n){
-     for(int i=0;i<n-1;i++){
-          int minidx=i;
-          for(int j=i+1;j<n;j++){
-            if(arr[j]<arr[minidx]){
-                minidx=j;
-            }
-
-
+#include <iostream>
+using namespace std;
+void selectionsort(int arr[], int n)
+{
+     for (int i = 0; i < n - 1; i++)
+     {
+          int minidx = i;
+          for (int j = i + 1; j < n; j++)
+          {
+               if (arr[j] < arr[minidx])
+               {
+                    minidx = j;
+               }
           }
-          swap(arr[i],arr[minidx]);
-
-
+          swap(arr[i], arr[minidx]);
      }
-     for(int i=0;i<n;i++){
-        cout<<arr[i]<<endl;
+     for (int i = 0; i < n; i++)
+     {
+          cout << arr[i] << endl;
      }
-
 }
 
+int main()
+{
+     int arr[5] = {5, 4, 1, 3, 2};
+     int n = sizeof(arr) / sizeof(arr[0]);
+     selectionsort(arr, n);
 
-
-
-int main(){
-  int arr[5]={5,4,1,3,2};
-  int n=sizeof(arr)/sizeof(arr[0]);
-  selectionsort(arr,n);
-
-
-
- return 0;   
+     return 0;
 }
-// hI my name is Manish
+// Hi my name is Rajat Bhagat
